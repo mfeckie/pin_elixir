6be643046e7b492727c6f3fb9233283101b3807d
Martin Feckie <mfeckie@gmail.com>
2015-10-18 12:34:44 +0800

    Add test watcher

--- mix.exs
+++ mix.exs
@@ -28,6 +28,8 @@ defmodule PinElixir.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [ 
+      {:mix_test_watch, "~> 0.2", only: :dev}
+    ]
   end
 end
