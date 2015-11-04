621225e337950768c291216cfcdae142eb8e7000
Martin Feckie <mfeckie@gmail.com>
2015-11-04 11:26:56 +0900

    Remove cruft from card and derive poison encoder

--- lib/cards/card.ex
+++ lib/cards/card.ex
@@ -3,16 +3,6 @@ defmodule PinElixir.Card do
              :cvc, :name, :address_line1, :address_postcode,
              :address_city, :address_state, :address_country,
              :token, :scheme, :display_number]
-
-  def from_map(string_key_map) do
-    atomic = for {key, value} <- string_key_map,
-    into: %{},
-    do: {String.to_atom(key), value}
-    to_struct(atomic)
-  end
-
-  defp to_struct(map) do
-    struct(PinElixir.Card, map)
-  end
+  @derive [Poison.Encoder]
 
 end
